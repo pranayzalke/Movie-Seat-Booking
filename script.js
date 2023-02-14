6b78e8a5e3615d330e65f7a3acc30a5b9a1f907e
Pranay Zalke <115355409+pranayzalke@users.noreply.github.com>
2023-02-14 10:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+let ticketPrice = +movieSelect.value;
+
+//5. get data from localstorage and update the UI.
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  
+   //to get data from localstorage for selectedMovieIndex
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+
+//4. The function is used to set the data in local storage ----------------------------------------------
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
+//3. Movie select event--------------------------------------------------------------
+ movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+
+  setMovieData(e.target.selectedIndex, e.target.value);
+});
+
+
+
+//2. update total no of selected seats and total cost----------------------------------------------------------
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+  
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+  
+  //adding data to local storage
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
+
+
+
+
+//1. Seat click event--------------------------------------------------------------------------------
+container.addEventListener('click',(e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// intial count and total
+updateSelectedCount();
